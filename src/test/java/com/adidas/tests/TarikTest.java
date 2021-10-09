e274d2f3dc8bea7a19aaa42c5b47b8dbda4c529b
demiralitt <alitarikdemir@gmail.com>
2021-10-09 18:36:37 +0200

    Tarik add 1 test

---
+++ src/test/java/com/adidas/tests/TarikTest.java
@@ -0,0 +1,12 @@
+package com.adidas.tests;
+
+import com.adidas.base.TestBase;
+import org.testng.annotations.Test;
+
+public class TarikTest extends TestBase {
+
+@Test
+    public void test1(){
+
+}
+}
