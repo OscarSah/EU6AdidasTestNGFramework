43a07e752c485cfdb0da33fdec7126e5bea6b203
OscarSah <oscar@cybertekschool.com>
2021-10-16 20:04:54 +0300

    Polymer Task finished

--- src/test/java/com/Polymer/pagesPolymer/PolymerPage.java
+++ src/test/java/com/Polymer/pagesPolymer/PolymerPage.java
@@ -1,6 +1,8 @@
 package com.Polymer.pagesPolymer;
 
 import com.Polymer.basePolymer.BasePagePolymer;
+import com.adidas.utilities.Driver;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
@@ -9,5 +11,13 @@ public class PolymerPage extends BasePagePolymer {
     @FindBy(id = "new-todo")
     public WebElement createItemBox;
 
+    @FindBy(xpath = "//h3[.='Polymer']")
+    public WebElement pageName;
 
+    @FindBy(id = "edit")
+    public WebElement editBox;
+
+    public WebElement createdItem(String str){
+        return Driver.get().findElement(By.xpath("//label[.='"+str+"']"));
+    }
 }
--- src/test/java/com/Polymer/testsPolymer/PolymerTest.java
+++ src/test/java/com/Polymer/testsPolymer/PolymerTest.java
@@ -3,8 +3,12 @@ package com.Polymer.testsPolymer;
 import com.Polymer.basePolymer.BasePagePolymer;
 import com.Polymer.basePolymer.TestBasePolymer;
 import com.Polymer.pagesPolymer.JavaScriptPage;
+import com.Polymer.pagesPolymer.PolymerPage;
 import com.adidas.base.BasePage;
 import com.adidas.utilities.Driver;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -22,7 +26,7 @@ Bonus: (optional stretch goal): Edit the content of the second Todo item
 
 
     JavaScriptPage javaScriptPage = new JavaScriptPage();
-
+    PolymerPage polymerPage = new PolymerPage();
     @Test
     public void test(){
         extentLogger = report.createTest("Polymer Test Started");
@@ -39,7 +43,34 @@ Bonus: (optional stretch goal): Edit the content of the second Todo item
         // select the Polymer link
         javaScriptPage.getLink("Polymer").click();
         extentLogger.info("select the Polymer link");
+        Assert.assertTrue(polymerPage.pageName.isDisplayed());
         Assert.assertTrue(driver.getTitle().contains("Polymer"));
         extentLogger.info("Verify that I am on the Polymer page");
+
+        String todo1 = "Happy Testing with EU6";
+        polymerPage.createItemBox.sendKeys(todo1+ Keys.ENTER);
+
+        String todo2 = "Second item for Happy Testing";
+        polymerPage.createItemBox.sendKeys(todo2+Keys.ENTER);
+
+        extentLogger.info("Added Two Items");
+        Assert.assertTrue(polymerPage.createdItem(todo2).isDisplayed());
+
+        // I create Actions object to handle double clicking and putting new text
+        Actions actions = new Actions(driver);
+
+        WebElement btnForChanging=polymerPage.createdItem(todo2);
+        actions.doubleClick(btnForChanging).perform();
+
+        for (int i = 0; i < todo2.length() ; i++) {
+            polymerPage.editBox.sendKeys(Keys.BACK_SPACE);
+        }
+
+        polymerPage.editBox.sendKeys("I got it Friends!"+Keys.ENTER);
+
+        Assert.assertTrue(polymerPage.createdItem("I got it Friends!").isDisplayed());
+
+        extentLogger.pass("I changed the second item");
+
     }
 }
