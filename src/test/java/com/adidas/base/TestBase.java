9c2f0974593ddc46bebb976c1e37a967d94db719
OscarSah <oscar@cybertekschool.com>
2021-10-16 18:15:47 +0300

    Before Test added

--- src/test/java/com/adidas/base/TestBase.java
+++ src/test/java/com/adidas/base/TestBase.java
@@ -2,11 +2,15 @@ package com.adidas.base;
 
 import com.adidas.utilities.ConfigurationReader;
 import com.adidas.utilities.Driver;
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
 
 import java.util.concurrent.TimeUnit;
 
@@ -19,6 +23,37 @@ public abstract class TestBase {
     protected Actions actions;
     protected WebDriverWait wait;
     protected String url;
+
+    protected static ExtentReports report;
+
+    protected static ExtentHtmlReporter htmlReporter;
+
+    protected static ExtentTest extentLogger;
+
+    @BeforeTest
+    public void setUpTest(){
+        //initialize the class
+        report = new ExtentReports();
+
+        //create a report path
+        String projectPath = System.getProperty("user.dir");
+        String path = projectPath + "/test-output/report.html";
+
+        //initialize the html reporter with the report path
+        htmlReporter = new ExtentHtmlReporter(path);
+
+        //attach the html report to report object
+        report.attachReporter(htmlReporter);
+
+        //title in report
+        htmlReporter.config().setReportName("Adidas Purchase Test");
+
+        //set environment information
+        report.setSystemInfo("Environment","QA");
+        report.setSystemInfo("Browser", ConfigurationReader.get("browser"));
+        report.setSystemInfo("OS",System.getProperty("os.name"));
+
+    }
 
     @BeforeMethod
     public void setUp(){
