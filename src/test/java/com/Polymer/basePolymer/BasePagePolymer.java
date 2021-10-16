a57e3021b6f21e80a286c9316cdbeda250348221
OscarSah <oscar@cybertekschool.com>
2021-10-16 19:27:16 +0300

    BasePage for Polymer added

--- src/test/java/com/Polymer/basePolymer/BasePagePolymer.java
+++ src/test/java/com/Polymer/basePolymer/BasePagePolymer.java
@@ -1,22 +1,22 @@
 package com.Polymer.basePolymer;
 
 import com.adidas.utilities.Driver;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-public abstract class BasePage {
+public abstract class BasePagePolymer {
 
 
-    public BasePage() {
+    public BasePagePolymer() {
         PageFactory.initElements(Driver.get(), this);
     }
 
-    @FindBy(partialLinkText = "Home")
-    public WebElement home;
+ public WebElement getElementTab(String str){
 
-    @FindBy(linkText = "Cart")
-    public WebElement cart;
-
+        String tab ="//div[.='"+str+"']";
+        return Driver.get().findElement(By.xpath(tab));
+ }
 
 }
