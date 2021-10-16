6017bb17153b6de600c2303a1a12421e0ac5fb3a
OscarSah <oscar@cybertekschool.com>
2021-10-16 18:36:35 +0300

    Test ready for running

--- src/test/java/com/adidas/tests/PurchaseTest.java
+++ src/test/java/com/adidas/tests/PurchaseTest.java
@@ -4,12 +4,13 @@ import com.adidas.base.BasePage;
 import com.adidas.base.TestBase;
 import com.adidas.pages.CartPage;
 import com.adidas.pages.ProductsPage;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class PurchaseTest extends TestBase {
 
     ProductsPage productPage = new ProductsPage(); //
-    BasePage cartPage = new CartPage(); // polymorphic way
+    CartPage cartPage = new CartPage(); // polymorphic way
 
 
     int expectedPurchaseAmount =0;
@@ -22,18 +23,38 @@ public class PurchaseTest extends TestBase {
 
    //     System.out.println("driver.getCurrentUrl() = " + driver.getCurrentUrl());
 
+        extentLogger = report.createTest("Adding and Removing Products Test");
+
+
+        extentLogger.info("Add items to Cart, and handle the alerts");
         String[][] purchaseInfo = new String[][]{{"Laptops","Sony vaio i5"},{"Laptops","Dell i7 8gb"},
                 {"Monitors","Apple monitor 24"},{"Phones","Nexus 6"}};
         for(String[] strings : purchaseInfo){
             expectedPurchaseAmount+=productPage.productAdder(strings[0],strings[1],wait);
         }
 
+        extentLogger.info("Remove unwanted products");
         String[] unwantedProductsInfo = {"Dell i7 8gb","Apple monitor 24"};
         for (String s : unwantedProductsInfo) {
-            expectedPurchaseAmount-= ((CartPage)cartPage).productRemover(s, wait); // I had to cast because created in poly way
+            expectedPurchaseAmount-= cartPage.productRemover(s, wait); // I had to cast because created in poly way
         }
 
         System.out.println("expectedPurchaseAmount = " + expectedPurchaseAmount);
+
+        extentLogger.info("Click on place order button");
+        cartPage.placeButton.click();
+
+        extentLogger.info("Fill the form for Fake customer");
+        cartPage.fillForm();
+
+        extentLogger.info("Click on purchase button");
+        cartPage.purchaseButton.click();
+
+        String confirmation = cartPage.confirmation.getText();
+
+        extentLogger.info("Verify the purchase amount");
+        Assert.assertTrue(confirmation.contains(""+expectedPurchaseAmount));
+
 
     }
 
