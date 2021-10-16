2cf27a3bd365165752c95f051bf888a67567c036
OscarSah <oscar@cybertekschool.com>
2021-10-16 19:12:19 +0300

    Polymer Started

---
+++ src/test/java/com/Polymer/testsPolymer/PolymerTest.java
@@ -0,0 +1,14 @@
+package com.Polymer.testsPolymer;
+
+public class PolymerTest {
+
+    /*
+Write a basic browser automation framework to validate a Polymer website.
+The focus should be on the interaction with the browser.
+The Web Application under test http://todomvc.com/
+The first step should be to load the website, click within the JavaScript tab,
+and select the Polymer link.
+The second step should be: Add two Todo items
+Bonus: (optional stretch goal): Edit the content of the second Todo item
+     */
+}
